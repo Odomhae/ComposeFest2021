3e95c8ffe5bbd0d1e42b063bc632efa7250eda44
unknown <jihoon708@naver.com>
2021-11-04 00:09:38 +0900

    Week1 -2nd /9강

--- week 1-Jetpack Compose basics/MyCodelabWeek1/app/src/main/java/com/odom/week1/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyCodelabWeek1/app/src/main/java/com/odom/week1/MainActivity.kt
@@ -3,22 +3,16 @@ package com.odom.week1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.odom.week1.ui.theme.Week1Theme
-import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,11 +28,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+fun MyApp(){
+
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding){
+        OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+    }else{
+        Greetings()
+    }
+
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Composable
+// 리스트 10개
+private fun Greetings(names: List<String> = List(10){"$it"}) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
+        items(items = names){name ->
             Greeting(name = name)
         }
+    }
+//    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+//        for (name in names) {
+//            Greeting(name = name)
+//        }
+//    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    Week1Theme {
+        OnboardingScreen(onContinueClicked = {}) // Do nothing on click.
     }
 }
 
@@ -56,6 +98,7 @@ private fun Greeting(name: String) {
     ) {
         // 가로로 weight 1
         Row(modifier = Modifier.padding(24.dp)) {
+            // 텍스트 2줄
             Column(modifier = Modifier
                 .weight(1f)
                 .padding(bottom = extraPadding)) {
@@ -64,6 +107,7 @@ private fun Greeting(name: String) {
                 Text(text = name)
             }
 
+            // 버튼
             OutlinedButton(
                 onClick = { expanded.value = !expanded.value }
             ) {
@@ -72,7 +116,6 @@ private fun Greeting(name: String) {
         }
     }
 }
-
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
