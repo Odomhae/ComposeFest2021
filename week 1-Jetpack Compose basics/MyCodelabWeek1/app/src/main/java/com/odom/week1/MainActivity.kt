10622d2922aa9e0bb00434644fcd4de00043058f
unknown <jihoon708@naver.com>
2021-11-04 23:17:31 +0900

    Week1 -3rd Fin

--- week 1-Jetpack Compose basics/MyCodelabWeek1/app/src/main/java/com/odom/week1/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyCodelabWeek1/app/src/main/java/com/odom/week1/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.odom.week1
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.odom.week1.ui.theme.Week1Theme
@@ -31,7 +42,9 @@ class MainActivity : ComponentActivity() {
 fun MyApp(){
 
     // TODO: This state should be hoisted
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    //var shouldShowOnboarding by remember { mutableStateOf(true) }
+    // remembr하면 화면회전이나 다크모드시 재시작됨
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding == true){
         // 초기화면
@@ -88,11 +101,78 @@ fun OnboardingPreview() {
 
 @Composable
 private fun Greeting(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+    }
+}
+
+/*
+@Composable
+private fun Greeting(name: String) {
     // To preserve state across recompositions, remember the mutable state using remember.
-    val expanded = remember{ mutableStateOf(false)}
+    var expanded by remember{ mutableStateOf(false)}
 
     // 펼쳐지면
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    // val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            // 마지막에 바운스
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            // 천천히
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -103,21 +183,48 @@ private fun Greeting(name: String) {
             // 텍스트 2줄
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)) {
+                // padding은 0이상 ..
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
 
                 Text(text = "Hello, ")
-                Text(text = name)
+                // 글자 스타일
+                Text(text = name, style = MaterialTheme.typography.h3.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
+                if (expanded){
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
+                IconButton(onClick = {expanded = !expanded}) {
+                    Icon(imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                        contentDescription = if(expanded){
+                            stringResource(id = R.string.show_less)}else{
+                                stringResource(id = R.string.show_more)
+                            } )
+
+                }
             }
 
             // 버튼
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if(expanded.value) "Show less" else "Show more")
-            }
+//            OutlinedButton(
+//                onClick = { expanded = !expanded }
+//            ) {
+//                Text(if(expanded) "Show less" else "Show more")
+//            }
         }
     }
 }
+*/
+
+// 다크모드 프리뷰
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
